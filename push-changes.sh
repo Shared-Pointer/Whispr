f4f2aa6cd357cb238890876128c2f8381c4bf1ee
Korbielowski <dawidstasinski12@gmail.com>
2025-02-15 15:48:21 +0100

    Update push-changes script

--- push-changes.sh
+++ push-changes.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
 git fetch origin $(git rev-parse --abbrev-ref HEAD)
 git add --interactive
+echo "Specify commit message:"
+read commit_message
+git commit -m "$commit_message"
+git push
