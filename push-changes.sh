692f07d9f4bb86ebd9fe5c68924b58cb99dddef5
Korbielowski <dawidstasinski12@gmail.com>
2025-02-15 15:54:52 +0100

    Update push-changes file vol. 2

--- push-changes.sh
+++ push-changes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git fetch origin $(git rev-parse --abbrev-ref HEAD)
+clang-format --Werror -style=file -i src/*.cpp include/*.h 
 git add --interactive
 echo "Specify commit message:"
 read commit_message
